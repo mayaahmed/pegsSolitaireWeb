a20aac74ba89414f0f6d635ed7ce8bcb932ee00e
Maya Mohsin Ahmed <maya.ahmed@gmail.com>
2016-11-05 11:14:52 +0530

    Add files via upload

--- pegs.js
+++ pegs.js
@@ -1,3 +1,4 @@
+var movesArray = new Array();
 status=0;  row1=0; row2=0; col1=0; col2=0; count=32;
 
 function initial(){
@@ -56,10 +57,11 @@ function run(){
   var test;
   test=checkValidMove();
   if(test==1){
+
   move();
   if (count==1){ 
    applaud();
-alert('Congratulations! You win');
+   // alert('Congratulations! You win');
  
 }
   
@@ -76,6 +78,7 @@ alert('Congratulations! You win');
 
 function move(){
   
+
   var pic1; var pic2; var pic3; var row3; var col3;
 pic1 = document.getElementById('marblePic'+row1+col1); 
 pic1.style.visibility="hidden";
@@ -87,12 +90,20 @@ if(row1==row2){
      pic3 = document.getElementById('marblePic'+row1+col3); 
      pic3.style.visibility="hidden";
      count=count-1;
+var moveObj= {r1: row1, r2: row2, r3: row1, c1: col1, c2: col2, c3: col3};
+movesArray.push(moveObj);
+length = movesArray.length-1;
+
   } 
   else if(col2==col1-2){
     col3=col1-1; 
      pic3 = document.getElementById('marblePic'+row1+col3); 
      pic3.style.visibility="hidden";
      count=count-1;
+var moveObj= {r1: row1, r2: row2, r3: row1, c1: col1, c2: col2, c3: col3};
+movesArray.push(moveObj);
+length = movesArray.length-1;
+
   }
 }
 else if(col1==col2){
@@ -101,16 +112,24 @@ else if(col1==col2){
      pic3 = document.getElementById('marblePic'+row3+col1); 
      pic3.style.visibility="hidden";
      count=count-1;
+var moveObj= {r1: row1, r2: row2, r3: row3, c1: col1, c2: col2, c3: col1};
+movesArray.push(moveObj);
+length = movesArray.length-1;
+
   } 
   else if(row2==row1-2){
     row3=row1-1; 
      pic3 = document.getElementById('marblePic'+row3+col1); 
      pic3.style.visibility="hidden";
      count=count-1;
+var moveObj= {r1: row1, r2: row2, r3: row3, c1: col1, c2: col2, c3: col1};
+movesArray.push(moveObj);
+length = movesArray.length-1;
+
   }
 }
 
- 
+
 }
 
 function checkValidMove(){
@@ -160,18 +179,32 @@ if(col1==col2-2){
     if(pic.style.visibility=="hidden") return 0;
   }
 }
-
-
-
-
 return 1;
 }
+
+function undo(){
+length = movesArray.length-1;
+
+pic1 = document.getElementById('marblePic'+movesArray[length].r1+movesArray[length].c1); 
+pic2 = document.getElementById('marblePic'+movesArray[length].r2+movesArray[length].c2); 
+pic3 = document.getElementById('marblePic'+movesArray[length].r3+movesArray[length].c3); 
+pic1.style.visibility="visible";
+pic3.style.visibility="visible";
+pic2.style.visibility="hidden";
+movesArray.pop(); count=count+1;
+}
+
+
+
 
 function applaud(){
 var audio = new Audio('applause.mp3');
 audio.play();
+var string = 'Congratulations! You win.';
+document.getElementById("victoryDiv").innerHTML = string;
 }
 
+// applaud();
 
 function openNav() {
     document.getElementById("myNav").style.height = "100%";
